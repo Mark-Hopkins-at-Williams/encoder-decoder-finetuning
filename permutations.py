a351b0a95700177ff018a340d236f5f0dbe68f23
yukina <yy5@williams.edu>
2025-06-26 16:49:17 -0400

    permutation part2

--- permutations.py
+++ permutations.py
@@ -30,28 +30,31 @@ class create_random_permutation_with_fixed_points:
 #function to save dictionary into json file
 def save_permutation_map(pmap, filename):
     with open(filename, 'w') as f:
+        all_data = {}
         for key,value in pmap.items():
-            data = {
-                "language": key,
+            all_data[key] = {
                 "fixed points": value.fixed_points,
                 "rng": value.num,
                 "result dictionary": value.result_dict,
                 "inverse dictionary": value.inverse_dict
             }
+        json.dump(all_data, f, indent=4)
 
-            json.dump(data, f, indent=4)
 
 #function to read json file
 def load_permutation_map(filename):
     with open(filename, 'r') as f:
         data_dict = json.load(f)
-        data = {
-                "language": data_dict[language],
-                "fixed points": data_dict,
-                "rng": value.num,
-                "result dictionary": value.result_dict,
-                "inverse dictionary": value.inverse_dict
-            }
+        data = {}
+        for key,value in data_dict.items():
+            obj = create_random_permutation_with_fixed_points(
+                num = value["rng"],
+                fixed_points = value["fixed points"]
+            )
+            obj.result_dict = {int(i):s for i, s in value["result dictionary"].items()}
+            obj.inverse_dict = {int(i):s for i, s in value["inverse dictionary"].items()}
+            data[key] = obj
+        return data
 
         
         
