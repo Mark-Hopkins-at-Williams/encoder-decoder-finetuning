f12eff26033b266a139805e377adfb361808ccb3
Denis Sotnichenko <ds23@williams.edu>
2025-07-14 14:26:29 -0400

    k

--- run.sh
+++ run.sh
@@ -5,8 +5,4 @@
 #SBATCH -o logs/log_%j.out
 #SBATCH -e logs/log_%j.err
 #SBATCH --gres=gpu:1
-<<<<<<< HEAD
-python permutations.py --model_dir exp --steps 60000
-=======
 python finetune.py --config examples/example1.json
->>>>>>> 93f6fd797e343b9b4301028ba2baf8d707c56c44
